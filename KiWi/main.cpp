a59de838783da30bf0daba2cd7fe8e40c54928c0
eitanhaimashiah <eitanhaimashiah@gmail.com>
2018-04-02 12:17:48 +0300

    Initial Commit

---
+++ KiWi/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  KiWi
+//
+//  Created by Eitan-Hai Mashiah on 2.4.2018.
+//  Copyright © 2018 eitan mashiah. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
